63190a42386c97a2dd7ce02430906d35d906846a
Isquare007 <akibrahiim2019@gmail.com>
2022-06-02 12:22:19 -0700

    6. size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,23 +3,20 @@
 /**
   * main - Entry point
   *
-  * Description: print sizes of constants
-  *
-  * Return: (0) success
+  * Return:Always 0 (success)
   */
-
 int main(void)
 {
-	char c;
-	int i;
-	long int k;
-	long long int l;
+	int a;
+	long int b;
+	long long int c; 
+	char d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(i));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(l));
-	printf("Size of a float: %lu bytes(s)\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
