7c9334c20e6fa311202b915119620fd0539bc6ed
Isquare007 <akibrahiim2019@gmail.com>
2022-06-02 07:30:05 -0700

    4. puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: program prints a text
+  *
+  * Return: Always returns (0)
+  */
+
+int main(void)
+{
+	puts(""Programming is like building a multilingual puzzle");
+	return (0);
+}
