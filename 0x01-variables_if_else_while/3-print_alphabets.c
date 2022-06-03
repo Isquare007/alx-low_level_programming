70cde7fae158a4b987886f10d4dee149b1ae8077
Isquare007 <akibrahiim2019@gmail.com>
2022-06-03 08:04:21 -0700

    3.print Alphabtes

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: print lowercase and uppercase
+  *
+  * Return: Always (0) success
+  */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	char c = 'A';
+
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
