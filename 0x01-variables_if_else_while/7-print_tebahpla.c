dba2e9db55a3b0d4fe8358425facb1e7728263f5
Isquare007 <akibrahiim2019@gmail.com>
2022-06-03 10:07:37 -0700

    7. print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: print alphabets in reverse
+  *
+  * Return: Always 0 (succes)
+  */
+int main(void)
+{
+	int rev;
+
+	for (rev = 'z'; rev => 'a'; c--)
+	{
+		putchar(rev);
+	}
+	putchar('\n');
+	return 0;
+}
