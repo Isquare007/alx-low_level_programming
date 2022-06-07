f0042c01a02e690a1f6e2e4a61d5a6e26b493bb5
Isquare007 <akibrahiim2019@gmail.com>
2022-06-07 08:08:00 -0700

    5. sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * _abs - absolute number
+  *
+  * Description: print absolute number of an integar
+  *
+  * @n: input integar
+  *
+  * Return: integar
+  */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
