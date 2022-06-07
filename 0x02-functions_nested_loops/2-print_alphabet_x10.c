fd904429a31a3e48e81f69eb741417915cc747b1
Isquare007 <akibrahiim2019@gmail.com>
2022-06-07 06:40:37 -0700

    2 print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,14 @@
   */
 void print_alphabet_x10(void)
 {
-	int i = 0, j;
+	int i, j;
 
-	while (i < 10)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 97; j < 123; j++)
+		{
 			_putchar(j);
+		}
 	}
 	_putchar('\n');
 }
