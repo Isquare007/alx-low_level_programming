243e2c1a7ab20de2c012d25ccadad92fde66852a
Isquare007 <akibrahiim2019@gmail.com>
2022-06-07 04:03:44 -0700

    0. putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * main - Entry point
+  *
+  * Description: prints _putchar
+  *
+  * Return: always (0) success
+  */
+
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
