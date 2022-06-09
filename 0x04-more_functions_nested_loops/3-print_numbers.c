2bd1dd2419df6c9168e8b3fb9e0cf5e54d5e351a
Isquare007 <akibrahiim2019@gmail.com>
2022-06-09 12:22:45 +0000

    3. print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * print_numbers - print numbers
+  *
+  * Return: 0
+  */
+void print_numbers(void)
+	int d
+{
+	for (d = 48; d <= 57; d++)
+	{
+		_putchar(d + '0');
+	}
+	_putchar('\n');
+}
