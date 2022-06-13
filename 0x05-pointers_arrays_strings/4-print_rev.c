19723fc567d2de9aa684e510999ae05c6dbf320b
Isquare007 <akibrahiim2019@gmail.com>
2022-06-13 21:18:07 +0000

    4. print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h'
+/**
+  * print_rev - print in reverse
+  * @s: string
+  * Return: reverse string
+  */
+void print_rev(char *s)
+{
+	int len = 0, i = 0;
+
+	while (s[i++])
+		len++;
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n')
+}
