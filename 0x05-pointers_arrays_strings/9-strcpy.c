5e7a3284633544b9d7a292275c9937fb619824af
Isquare007 <akibrahiim2019@gmail.com>
2022-06-13 22:50:15 +0000

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _strcpy - string copy pointed bt src, including the terminating null byte
+  * @src: src
+  * @dest: buffer to copy the string to 
+  * Return: dest
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index] != '\0'; index++)
+	{
+		dest[index] = src[index];
+	}
+	dest[index] = '\0';
+	return (dest)
+}
