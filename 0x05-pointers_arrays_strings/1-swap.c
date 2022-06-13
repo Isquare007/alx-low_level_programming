641870555d8c870032e58ba1c2e25b728eeee4c4
Isquare007 <akibrahiim2019@gmail.com>
2022-06-13 12:54:52 +0000

    1. swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * swap_int - swap two integars
+  * @a: first integar
+  * @b: second integar
+  *
+  * Return: swapped integars
+  */
+void swap_int(int *a, int *b)
+{
+	int a;
+	int b;
+
+	*a = b;
+	*b = a;
+}
