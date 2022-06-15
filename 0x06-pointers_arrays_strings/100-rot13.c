75f0855fedc270767fa27fdae2f5d61613b2c204
Isquare007 <akibrahiim2019@gmail.com>
2022-06-15 21:27:51 +0000

    100.Rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * rot13 - Rotate by 13 places
+  * @s: string
+  * Return: s
+  */
+char *rot13(char *s)
+{
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "MNOPQRSTUVWXYZABCDEFGHIJKLmnopqrstuvwxyzabcdefghijkl";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[i];
+				break;
+			}
+		}
+	}
+	return (s);
+}
