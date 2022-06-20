0feddcdb52ba554a67397e2b5805893655ebf11c
Isquare007 <akibrahiim2019@gmail.com>
2022-06-20 13:42:39 +0000

    0.memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * _memset - fills up the memory
+  * @s: the point to memory area
+  * @b: constant byte
+  * @n: number of bytes
+  *
+  * Return: s
+  */
+char *_memset(char *s, char b, unsigned int n);
+{
+	int i;
+
+	while (n > 0)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
+	return (s);
+}
