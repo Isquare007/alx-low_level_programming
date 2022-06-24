d46a253c9357243b997bcaba8b4a9f266387b8f3
Isquare007 <akibrahiim2019@gmail.com>
2022-06-24 09:00:19 +0000

    arguementcount

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - Entry point
+  * @argc: argurment count
+  * @argv: arguement vector
+  * Return: void
+  */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
