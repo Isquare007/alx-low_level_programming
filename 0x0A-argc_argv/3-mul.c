fc89392b723e8ea3f75177c32d12dbeafef99ba8
Isquare007 <akibrahiim2019@gmail.com>
2022-06-24 10:20:13 +0000

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * main - Entry point
+  * @argv: arguement vector
+  * @argc:arguement count
+  * Return: void or return 1 if error
+  */
+int main(int argc, char *argv[])
+{
+	int x, y;
+	int mul;
+
+	if(argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+
+	mul = x * y;
+
+	printf("%d\n", mul);
+
+	return (0);
+}
