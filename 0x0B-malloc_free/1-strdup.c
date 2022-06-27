51b139a9fdcddc04e6d1088a68434b7a1f93d1c6
Isquare007 <akibrahiim2019@gmail.com>
2022-06-27 13:15:01 +0000

    1. strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _strdup - returns a pointer to an allocated spring
+  * @str: string
+  * Return: pointer or NULL
+  */
+char *_strdup(char *str)
+{
+	int i;
+	int *ar;
+	int size;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = str / str[0];
+
+	ar = malloc(size * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ar[i] = str[i];
+	return (ar);
+}
