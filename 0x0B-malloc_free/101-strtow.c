9136b2f169e41ce7b41fd50bdb6ee722e5a225e3
Isquare007 <akibrahiim2019@gmail.com>
2022-06-27 22:18:27 +0000

    2. str concat

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,76 +1,102 @@
 #include "main.h"
 #include <stdlib.h>
-/**
-  * wordcountrec - count num of words recursively
-  * @str: pointer to char
-  * @i: current index
-  * Return: number of words
-  */
-int wordcountrec(char *str, int i)
-{
-	if (str[i] == '\0')
-		return (0);
 
-	if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
-		return (1 + wordcountrec(str, i + 1));
-	return (wordcountrec(str, i + 1));
-}
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
+
 /**
-  * wordcounter - counts the number of words in 1d array
-  * @str: pointer to char
-  * Return: number of words
-  */
-int wordcounter(char *str)
+   * word_len - Locates the index marking the end of the
+    *            first word contained within a string.
+     * @str: The string to be searched.
+      *
+       * Return: The index marking the end of the initial word pointed to by str.
+        */
+int word_len(char *str)
 {
-	if (str[0] != ' ')
-		return (1 + wordcountrec(str, 0));
-	return (wordcountrec(str, 0));
+		int index = 0, len = 0;
+
+			while (*(str + index) && *(str + index) != ' ')
+					{
+								len++;
+										index++;
+											}
+
+				return (len);
 }
+
 /**
-  * strtow - splits strings to word
-  * @str: string
-  * Return: NULL or pointer
-  */
+   * count_words - Counts the number of words contained within a string.
+    * @str: The string to be searched.
+     *
+      * Return: The number of words contained within str.
+       */
+int count_words(char *str)
+{
+		int index = 0, words = 0, len = 0;
+
+			for (index = 0; *(str + index); index++)
+						len++;
+
+				for (index = 0; index < len; index++)
+						{
+									if (*(str + index) != ' ')
+												{
+																words++;
+																			index += word_len(str + index);
+																					}
+										}
+
+					return (words);
+}
+
+/**
+   * strtow - Splits a string into words.
+    * @str: The string to be split.
+     *
+      * Return: If str = NULL, str = "", or the function fails - NULL.
+       *         Otherwise - a pointer to an array of strings (words).
+        */
 char **strtow(char *str)
 {
-	char **ar;
-	int i, n, m, words;
+		char **strings;
+			int index = 0, words, w, letters, l;
 
-	if (str == NULL || str[0] == 0)
-		return (NULL);
-	words = wordcounter(str);
-	if (words < 1)
-		return (NULL);
-	ar = malloc(sizeof(char *) * (words + 1));
-	if (ar == NULL)
-		return (NULL);
-	i = 0;
-	while (i < words && *str != '\0')
-	{
-		if (*str != ' ')
-		{
-			n = 0;
-			while (str[n] != ' ')
-				n++;
-			ar[i] = malloc(sizeof(char) * (n + 1));
-			if (ar[i] == NULL)
-			{
-				while (--i >= 0)
-					free(ar[--i]);
-				free(ar);
-				return (NULL);
-			}
-			m = 0;
-			while (m < n)
-			{
-				ar[i][m] = *str;
-				m++, str++;
-			}
-			ar[i][m] = '\0';
-			i++;
-		}
-		str++;
-	}
-	ar[i] = '\0';
-	return (ar);
+				if (str == NULL || str[0] == '\0')
+							return (NULL);
+
+					words = count_words(str);
+						if (words == 0)
+									return (NULL);
+
+							strings = malloc(sizeof(char *) * (words + 1));
+								if (strings == NULL)
+											return (NULL);
+
+									for (w = 0; w < words; w++)
+											{
+														while (str[index] == ' ')
+																		index++;
+
+																letters = word_len(str + index);
+
+																		strings[w] = malloc(sizeof(char) * (letters + 1));
+
+																				if (strings[w] == NULL)
+																							{
+																											for (; w >= 0; w--)
+																																free(strings[w]);
+
+																														free(strings);
+																																	return (NULL);
+																																			}
+
+																						for (l = 0; l < letters; l++)
+																										strings[w][l] = str[index++];
+
+																								strings[w][l] = '\0';
+																									}
+										strings[w] = NULL;
+
+											return (strings);
 }
