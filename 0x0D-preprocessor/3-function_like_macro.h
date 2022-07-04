0be27cdfa8777446c241f69cf8fefd81b2cd5dbb
Isquare007 <akibrahiim2019@gmail.com>
2022-07-04 11:37:55 +0000

    2-main.c

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+  * file - function like macro
+  * @x: number
+  * Description: find absolute number of a number
+  */
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+
+#endif
