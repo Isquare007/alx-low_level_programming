aed390cc5de01a33eaa4fe36af10b742f16da363
Isquare007 <akibrahiim2019@gmail.com>
2022-07-04 11:12:25 +0000

    0-objectlike & 1. PI

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,10 +1,12 @@
-#ifndef OBJECTLIKE_H
-#define OBJECTLIKE_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
   * file - 0-object file like macro
   * Description: create a header file that defines SIZE as an abbreviation 1024
   */
-#ifndef SIZE
+#define SIZE
+
 #define SIZE 1024
+
 #endif
