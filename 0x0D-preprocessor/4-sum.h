60a8ab3f41fff95c36337aff465eea62a7b50660
Isquare007 <akibrahiim2019@gmail.com>
2022-07-04 11:43:03 +0000

    4.sum

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+  * file - sum macro function like
+  * @x: first integar
+  * @y: second integar
+  * Description: sum two integars
+  */
+#define SUM(x, y) ((x) + (y))
+
+#endif
