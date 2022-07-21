35cab8b6b3c454764836943d66c702a36cbaddce
Isquare007 <akibrahiim2019@gmail.com>
2022-07-21 14:26:46 +0000

    0-binary to unit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: holberton.h
- * AUth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
- */
+#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
