8fbad99023f19c18d40e4784110833da28985d00
Isquare007 <akibrahiim2019@gmail.com>
2022-09-22 17:06:39 +0000

    hash_t

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables"
+
+/**
+ * hash_table_create - function that creates a hash table
+ * @size: size of array
+ * Return: a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_t;
+	unsigned long int i;
+
+	hash_t = malloc(sizeof(hash_table_t));
+	if (hash_t == NULL)
+		return (NULL);
+
+	hast_t->size = size;
+	hash_t->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (hash_t->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		hash_t->array[i] = NULL;
+
+	return (hash_t);
+}
