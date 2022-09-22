939f91b19215df4897dd9126ece83a568a6a9210
Isquare007 <akibrahiim2019@gmail.com>
2022-09-22 22:27:01 +0000

    hash table

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,10 +1,10 @@
 #include "hash_table.h"
 
 /**
- * key_index - get the index at which a key/value pair shold be sorted in array of hash table
+ * key_index - get the index at which a
+ * key/value pair shold be sorted in array of hash table
  * @key: the key to get index
  * @size: the size of the array
- * 
  * Return: The key's index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
