0b8e3d4677bc48bbc91064eba021cb868e11b884
Isquare007 <akibrahiim2019@gmail.com>
2022-11-09 22:36:40 +0000

    Files

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+Technical interview
+"""
+
+def island_perimeter(grid):
+    """
+    grid is a list of list of integers:
+    0 represents a water zone
+    1 represents a land zone
+    One cell is a square with side length 1
+    Grid cells are connected horizontally/vertically (not diagonally).
+    Grid is rectangular, width and height don’t exceed 100
+    """
+    width = len(grid[0])
+    height = len(grid)
+    grid_edges = 0
+    grid_size = 0
+
+    for m in range(height):
+        for n in range(width):
+            if(grid[m][n] == 1):
+                grid_size += 1
+                if (n > 0 and grid[m][n - 1] == 1):
+                    grid_edges += 1
+                if (m > 0 and grid[m - 1][n] == 1):
+                    grid_edges += 1
+    return (grid_size * 4 - grid_edges * 2)
