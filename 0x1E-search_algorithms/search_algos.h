2743d8056480b1ddd9b3c5d2cd6d8c9e804118d3
Isquare007 <akibrahiim2019@gmail.com>
2023-01-09 08:38:45 +0000

    fixed

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,16 +7,6 @@
 #include <stddef.h>
 #include <math.h>
 
-/**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- */
-
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
